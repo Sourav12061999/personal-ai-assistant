5149fbb1ba48e81e2e5ce024d972efee49f113eb
Sourav12061999 <sdunity12061999@gmail.com>
2024-02-12 08:48:28 +0530

    account permissions complete

--- backend/src/models/gmailLinks.model.ts
+++ backend/src/models/gmailLinks.model.ts
@@ -2,8 +2,10 @@ import { Schema, model } from "mongoose";
 
 const GmailLinkSchema = new Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true },
-    name: { type: String, required: false }
+    userID: { type: Schema.ObjectId, required: true, ref: 'users' },
+    token: { type: String, required: true },
+    expiry: { type: Number, required: true, },
+    refreshToken: { type: String, required: false }
 });
 
 const GmailLinkModel = model("gmail_link", GmailLinkSchema);
--- backend/src/routes/permission.route.ts
+++ backend/src/routes/permission.route.ts
@@ -1,32 +1,52 @@
 
 import { google } from "googleapis"
-
 import { Router } from "express";
 import { authenticateWithGoogle } from "../utils";
 import { FRONTEND_URL, JWT_SECRET } from "../env";
 import { verify } from "jsonwebtoken";
+import GmailLinkModel from "../models/gmailLinks.model";
 const router = Router();
 
 
 const DEFAULT_PATH = "/"
 router.get('/callback/', async (req, res) => {
-    const { code, state, ...rest } = req.query;
-    if (!state || typeof state !== "string") {
-        return res.status(404).send("Unauthenticated User");
+    try {
+        const { code, state, ...rest } = req.query;
+        if (!state || typeof state !== "string") {
+            return res.status(404).send("Unauthenticated User");
+        }
+        const _id = verify(state, JWT_SECRET);
+
+        if (typeof code !== "string") {
+            return res.status(404).json({
+                message: "Invalid Code",
+            });;
+        }
+        const gooleData = await authenticateWithGoogle(code);
+        if (!gooleData.data.emailAddresses) return;
+        console.log("Here is the goole data", gooleData.tokens);
+        const gmailLink = await GmailLinkModel.findOne({ email: gooleData.data.emailAddresses[0].value });
+        if (gmailLink) {
+            await GmailLinkModel.findByIdAndUpdate(gmailLink._id, {
+                token: gooleData.tokens.access_token, expiry: gooleData.tokens.expiry_date,
+                refreshToken: gooleData.tokens.refresh_token
+            },);
+        } else {
+            await GmailLinkModel.create({
+                email: gooleData.data.emailAddresses[0].value,
+                userID: _id,
+                token: gooleData.tokens.access_token,
+                expiry: gooleData.tokens.expiry_date,
+                refreshToken: gooleData.tokens.refresh_token
+            });
+        }
+        res.redirect(FRONTEND_URL!);
     }
-    // const _id = verify(state, JWT_SECRET);
-    
-    if (typeof code !== "string") {
-        return res.status(404).json({
-            message: "Invalid Code",
-        });;
+    catch (error) {
+        res.status(500).json({ message: "some error happened" });
     }
-    const gooleData = await authenticateWithGoogle(code);
-    console.log("Here is the goole data", gooleData);
-    
-    res.redirect(FRONTEND_URL!);
 });
 
 
-export default {router, defaultPath: DEFAULT_PATH};
+export default { router, defaultPath: DEFAULT_PATH };
 
